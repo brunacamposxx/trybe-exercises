2449db9f90cf27040e04c6f6fb83dcc6a40d757b
Bruna Campos <92brunacampos@gmail.com>
2021-08-10 15:15:03 -0300

    commit para unir branch

--- back-end/block_21/day_2/exists.sql
+++ back-end/block_21/day_2/exists.sql
@@ -14,7 +14,6 @@ where
       b.id = book_id
   );
 	
-	
 	-- Usando o EXISTS na tabela books_lent e books , exiba o id e título dos livros estão atualmente emprestados e que contêm a palavra "lost" no título.
 
 select
