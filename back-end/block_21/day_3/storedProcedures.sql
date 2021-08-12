9b1159d3a4277355f4c704baa452915b24b7f4a2
Bruna Campos <92brunacampos@gmail.com>
2021-08-11 22:52:45 -0300

    procedure sem parametros

--- back-end/block_21/day_3/storedProcedures.sql
+++ back-end/block_21/day_3/storedProcedures.sql
@@ -1,1 +1,19 @@
-teste novo nome de usuario no github
+-- Exercício 1:
+-- Monte uma procedure que exiba os 10 atores mais populares, baseado em sua quantidade de filmes. Essa procedure não deve receber parâmetros de entrada ou saída e, quando chamada, deve exibir o id do ator ou atriz e a quantidade de filmes atuados.
+USE sakila;
+delimiter $$
+create procedure ShowMostPopularActors() BEGIN
+select
+  actor_id,
+  count(film_id)
+from
+  film_actor
+group by
+  actor_id
+order by
+  count(film_id) desc
+limit
+  10;
+end $$
+delimiter;
+call ShowMostPopularActors
