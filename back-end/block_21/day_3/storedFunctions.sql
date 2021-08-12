fb449d22d494b9f92a1961707986d0be18e8eb3a
Bruna Campos <92brunacampos@gmail.com>
2021-08-12 17:17:45 -0300

    reads sql data - create function

--- back-end/block_21/day_3/storedFunctions.sql
+++ back-end/block_21/day_3/storedFunctions.sql
@@ -45,3 +45,27 @@ DELIMITER ;
 SELECT ReturnsFilmNameById(2); 
 
 
+
+
+-- Crie uma function que receba uma determinada categoria de filme em formato de texto (ex: 'Action' , 'Horror' ) e retorna a quantidade total de filmes registrados nessa categoria.
+
+USE sakila;
+DELIMITER $$
+
+CREATE FUNCTION CountCategoryFilm(category VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE movie_amount INT;
+    SELECT COUNT(*)
+    FROM sakila.category as c
+    INNER JOIN sakila.film_category as fc
+    ON fc.category_id = c.category_id
+    WHERE c.name = category INTO movie_amount;
+    RETURN movie_amount;
+END $$
+
+DELIMITER ;
+
+-- Como usar:
+
+SELECT CountCategoryFilm('Comedy');
