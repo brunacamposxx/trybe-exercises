2248b95767da8d9096cca1c2855d20d2346bea22
Bruna Campos <92brunacampos@gmail.com>
2021-08-12 16:00:37 -0300

    stored functions reads sql data

--- back-end/block_21/day_3/storedFunctions.sql
+++ back-end/block_21/day_3/storedFunctions.sql
@@ -18,3 +18,30 @@ DELIMITER ;
 -- Como usar:
 
  SELECT ReturnsTotalPayments(1);
+
+
+
+ -- Crie uma function que, dado o parâmetro de entrada inventory_id , retorna o nome do filme vinculado ao registro de inventário com esse id.
+
+ USE sakila;
+DELIMITER $$
+
+CREATE FUNCTION ReturnsFilmNameById(id INT)
+RETURNS VARCHAR(300) READS SQL DATA
+BEGIN
+    DECLARE movie_title VARCHAR(300);
+    SELECT distinct film.title
+    FROM sakila.inventory as inventory
+    INNER JOIN sakila.film as film
+    ON film.film_id = inventory.film_id
+    WHERE inventory.inventory_id = id INTO movie_title;
+    RETURN movie_title;
+END $$
+
+DELIMITER ;
+
+-- Como usar:
+
+SELECT ReturnsFilmNameById(2); 
+
+
