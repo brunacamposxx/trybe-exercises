52c247f15e5d3b7bf7f4440900d24fe4714f4368
Bruna Campos <92brunacampos@gmail.com>
2021-09-02 16:39:15 -0300

    subindo para pegar em outro notebook

---
+++ back-end/block_26/day_2.js
@@ -0,0 +1,4 @@
+// Aula ao vivo
+const multiplicar = (a, b) => a * b;
+const calcularPorcentagem = (a, b) => multiplicar (a, b/10);
+console.log(calcularPorcentagem(100, 200, 100));
---
+++ back-end/block_26/teste.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+const content = fs.readFile('script.md, (err, content) {
+	
+})
